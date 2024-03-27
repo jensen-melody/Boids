0016bf6d8697ab80a408ccef0be779344c579ec1
Melody Jensen <jajensen05@gmail.com>
2024-03-26 21:39:33 -0400

    26.3.2024

--- Boids/main.c
+++ Boids/main.c
@@ -2,10 +2,14 @@
 #include <SDL.h>
 #include "./constants.h"
 
+//Global variables used for things :3
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-int isRunning = false;
 
+int isRunning = false;
+int lr = 1;
+
+//ball structure for placing rectangles
 struct ball {
 	float x;
 	float y;
@@ -13,24 +17,34 @@ struct ball {
 	float height;
 }ball;
 
+//Setup window and it's parameters
 int initializeWindow(void) {
+	//Initializes SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		fprintf(stderr, "Error initializing SDL.\n");
 			return false;
 	}
+
+	//Window variable setup
 	window = SDL_CreateWindow(
-		"Gay Sex",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		windowWidth,
-		windowHeight,
-		SDL_WINDOW_ALLOW_HIGHDPI
+		"Gay Sex", //Window Title
+		SDL_WINDOWPOS_CENTERED, //Xpos
+		SDL_WINDOWPOS_CENTERED, //Ypos
+		windowWidth, //Width
+		windowHeight, //Height
+		SDL_WINDOW_ALLOW_HIGHDPI //Flags
 	);
+
+	//Checks if window initialized correctly
 	if (!window) {
 		fprintf(stderr, "Error creating SDL Window.\n");
 		return false;
 	}
+	
+	//Renderer variable setup 
 	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	//Checks if renderer initialized correctly
 	if (!renderer) {
 		fprintf(stderr, "Error creating SDL Renderer.\n");
 		return false;
@@ -39,23 +53,31 @@ int initializeWindow(void) {
 	return true;
 }
 
+//Sets up the scene
 void setup() {
 	//TODO: Setup
-	ball.x = 20;
-	ball.y = 20;
-	ball.width = 15;
-	ball.height = 15;
+
+	//Sets ball variables
+	ball.x = 0;
+	ball.y = 0;
+	ball.width = 400;
+	ball.height = 600;
 }
 
+//Takes and processes inputs
 int processInput() {
+	//init events
 	SDL_Event event;
 	SDL_PollEvent(&event);
 
+	//check if certain keys are pressed
 	switch (event.type) {
+		//if Exit, Alt+F4, or Win button is pressed
 		case SDL_QUIT: 
 			printf("Quit");
 			return false;
 			break;
+		//if X is pressed
 		case SDL_KEYDOWN:
 			if (event.key.keysym.sym == SDLK_ESCAPE)
 				printf("Esc");
@@ -65,40 +87,53 @@ int processInput() {
 	}
 }
 
+//Update objects every frame
 void update() {
-	//TODO: Update
+	if (ball.x >= windowWidth - ball.width) {
+		//todo
+	}
 }
 
+//Tell renderer to show objects on screen
 void render() {
+	//Draws background color
 	SDL_SetRenderDrawColor(renderer, 245, 169, 184, 255);
+
+	//Clears screen
 	SDL_RenderClear(renderer);
 
 	//Draw Rectangle
-	SDL_Rect ball_rect = { ball.x, ball.y, ball.width, ball.height };
+	SDL_Rect rectangle = { ball.x, ball.y, ball.width, ball.height };
 
 	SDL_SetRenderDrawColor(renderer, 91, 206, 250, 255);
-	SDL_FillRect(renderer, &ball_rect);
+	SDL_RenderFillRect(renderer, &rectangle);
 
+	//Swap buffer frame for current frame (Draws frame)
 	SDL_RenderPresent(renderer);
 }
 
+//Uninitializes everything in reverse order it initialized it
 void destroyWindow() {
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 }
 
+//main function :D
 int main(int argc, char* argv[]) {
+	//set isRunning variable to true
 	int isRunning = initializeWindow();
 
 	setup();
 
+	//Main Game Loop
 	while (isRunning) {
 		isRunning = processInput();
 		update();
 		render();
 	}
 
+	//If isRunning is false, or loop breaks suddenly, destroy the window and uninitialize everything
 	destroyWindow();
 
 	return false;
